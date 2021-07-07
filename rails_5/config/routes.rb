322f8e6316d9576ceefce8a440b064efe30c5d14
nogamishinpei <nogamichiru@gmail.com>
2021-07-07 15:49:20 +0900

    [Finish]2-5

---
+++ rails_5/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'todolists/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ get 'top'=>'homes#top'
+
+  post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
+
+  #.../todolist/1や.../todolist/3　に該当する
+ get 'todolists/:id' => 'todolists#show', as: 'todolist'
+
+end
